245a1bfa570f497dc517cf9bceb839cfad04c8c4
Valery Bermudez <139395288+ValeryBermudez@users.noreply.github.com>
2024-02-17 09:45:02 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Compraventa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblTitle = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.LblTipo = new System.Windows.Forms.Label();
+            this.LblPlaca = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LblModelo = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.LblPasajeros = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.LblObservaciones = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblTitle
+            // 
+            this.LblTitle.AutoSize = true;
+            this.LblTitle.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LblTitle.Location = new System.Drawing.Point(201, 9);
+            this.LblTitle.Name = "LblTitle";
+            this.LblTitle.Size = new System.Drawing.Size(121, 22);
+            this.LblTitle.TabIndex = 0;
+            this.LblTitle.Text = "Compraventa";
+            this.LblTitle.Click += new System.EventHandler(this.LblTitle_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(66, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // LblTipo
+            // 
+            this.LblTipo.AutoSize = true;
+            this.LblTipo.Location = new System.Drawing.Point(22, 71);
+            this.LblTipo.Name = "LblTipo";
+            this.LblTipo.Size = new System.Drawing.Size(33, 15);
+            this.LblTipo.TabIndex = 2;
+            this.LblTipo.Text = "Tipo:";
+            this.LblTipo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LblPlaca
+            // 
+            this.LblPlaca.AutoSize = true;
+            this.LblPlaca.Location = new System.Drawing.Point(22, 116);
+            this.LblPlaca.Name = "LblPlaca";
+            this.LblPlaca.Size = new System.Drawing.Size(38, 15);
+            this.LblPlaca.TabIndex = 3;
+            this.LblPlaca.Text = "Placa:";
+            this.LblPlaca.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 4;
+            // 
+            // LblModelo
+            // 
+            this.LblModelo.AutoSize = true;
+            this.LblModelo.Location = new System.Drawing.Point(211, 116);
+            this.LblModelo.Name = "LblModelo";
+            this.LblModelo.Size = new System.Drawing.Size(51, 15);
+            this.LblModelo.TabIndex = 5;
+            this.LblModelo.Text = "Modelo:";
+            this.LblModelo.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(268, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // LblPasajeros
+            // 
+            this.LblPasajeros.AutoSize = true;
+            this.LblPasajeros.Location = new System.Drawing.Point(22, 162);
+            this.LblPasajeros.Name = "LblPasajeros";
+            this.LblPasajeros.Size = new System.Drawing.Size(59, 15);
+            this.LblPasajeros.TabIndex = 7;
+            this.LblPasajeros.Text = "Pasajeros:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(93, 162);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 19);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "2";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(130, 162);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 19);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "4";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // LblObservaciones
+            // 
+            this.LblObservaciones.AutoSize = true;
+            this.LblObservaciones.Location = new System.Drawing.Point(22, 203);
+            this.LblObservaciones.Name = "LblObservaciones";
+            this.LblObservaciones.Size = new System.Drawing.Size(87, 15);
+            this.LblObservaciones.TabIndex = 10;
+            this.LblObservaciones.Text = "Observaciones:";
+            this.LblObservaciones.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(115, 203);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(0, 23);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(115, 203);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(367, 97);
+            this.textBox4.TabIndex = 12;
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.Location = new System.Drawing.Point(22, 350);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.BtnGuardar.TabIndex = 13;
+            this.BtnGuardar.Text = "Guardar";
+            this.BtnGuardar.UseVisualStyleBackColor = true;
+            this.BtnGuardar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.BtnGuardar);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.LblObservaciones);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.LblPasajeros);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.LblModelo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LblPlaca);
+            this.Controls.Add(this.LblTipo);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.LblTitle);
+            this.Name = "Form1";
+            this.Text = "CompraVenta";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label LblTitle;
+        private ComboBox comboBox1;
+        private Label LblTipo;
+        private Label LblPlaca;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private TextBox textBox1;
+        private Label LblModelo;
+        private TextBox textBox2;
+        private Label LblPasajeros;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label LblObservaciones;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button BtnGuardar;
+    }
+}
